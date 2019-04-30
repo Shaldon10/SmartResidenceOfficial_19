47f8045b3f199bbf581b8e3434990537029fc7a8
Shaldon Wade Naidoo <21610157@dut4life.ac.za>
2019-04-30 17:06:03 +0200

    Add project files.

---
+++ SmartResOfficial19/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SmartResOfficial19.Startup))]
+namespace SmartResOfficial19
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
